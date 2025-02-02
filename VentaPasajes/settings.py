1a64ab1fdb9cb1c65a80f3478aadd567bd9663e6
Diamante47 <137074345+Diamante47@users.noreply.github.com>
2025-02-01 22:14:04 -0500

    Database

--- VentaPasajes/settings.py
+++ VentaPasajes/settings.py
@@ -85,7 +85,9 @@ WSGI_APPLICATION = 'VentaPasajes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
 
 
 # Password validation
